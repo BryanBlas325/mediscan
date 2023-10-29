0b560dbc0cd813c7304aa6c138e324fa05dc481d
BryanBlas325 <brayananthony129@gmail.com>
2023-10-28 19:26:42 -0500

    primera subida

---
+++ src/main/java/com/cibertec/edu/pe/models/Pregunta.java
@@ -0,0 +1,5 @@
+package com.cibertec.edu.pe.models;
+
+public class Pregunta {
+
+}
---
+++ src/main/java/com/cibertec/edu/pe/models/Usuario.java
@@ -0,0 +1,94 @@
+package com.cibertec.edu.pe.models;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombres;
+	private String apellidos;
+	private int nro_documento;
+	private int edad;
+	private String fecha_pago;
+	private String estado;
+	
+	public Usuario(int id, String nombres, String apellidos, int nro_documento, int edad, String fecha_pago,
+			String estado) {
+		super();
+		this.id = id;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.nro_documento = nro_documento;
+		this.edad = edad;
+		this.fecha_pago = fecha_pago;
+		this.estado = estado;
+	}
+
+	public Usuario() {
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getNro_documento() {
+		return nro_documento;
+	}
+
+	public void setNro_documento(int nro_documento) {
+		this.nro_documento = nro_documento;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getFecha_pago() {
+		return fecha_pago;
+	}
+
+	public void setFecha_pago(String fecha_pago) {
+		this.fecha_pago = fecha_pago;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
