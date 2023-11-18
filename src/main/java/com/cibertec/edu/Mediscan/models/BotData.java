bf788bbe40a02a1c5ff38fa2e8adbe0f9bfcbaf8
BryanBlas325 <113562469+BryanBlas325@users.noreply.github.com>
2023-11-17 23:58:32 -0500

    Update BotData.java

--- src/main/java/com/cibertec/edu/Mediscan/models/BotData.java
+++ src/main/java/com/cibertec/edu/Mediscan/models/BotData.java
@@ -1,29 +1,32 @@
 package com.cibertec.edu.Mediscan.models;
 
 import java.io.Serializable;
-import java.time.LocalTime;
+import java.util.Set;
 
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name="bot_data")
 @Data
+@Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
-public class BotData implements Serializable {
-	
+public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String nombres;
+	private String apellidos;
+	private String correo;
+	private String contraseña;
+	private String idCompra;
 	
-	private String message;
+	private String fecha_pago;
+	private String estado;
 	
-	@ManyToOne
-    @JoinColumn(name="user_id", nullable=false)
-	private Usuario user;
-	
-	private String createAt;
+	@OneToMany(mappedBy="user")
+	@Transient
+	private Set<BotData>botData;
 }
